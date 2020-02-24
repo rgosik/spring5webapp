0315c5df030c301a575be26275f0d2d7e6284bfe
Rafal <grafal96@gmail.com>
2020-02-24 21:15:05 +0100

    author and book controllers added, thymeleaf views

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -37,8 +37,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher=" + publisher +
-                ", authors=" + authors +
                 '}';
     }
 
